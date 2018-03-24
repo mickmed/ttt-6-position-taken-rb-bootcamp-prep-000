2fb4e113d5de3fe786269668cd2f25f56fd35c56
Michael Roth <mick2090@gmail.com>
2018-03-24 11:20:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if (board[index] == " "  || board[index] == "" || board[index] == nil)
     return FALSE
-  elsif board[index] == "X" || board[index == "O"]
+  elsif (board[index] == "X" || board[index] == "O")
     return true
   end
 end
